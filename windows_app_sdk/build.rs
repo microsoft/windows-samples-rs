c63115ec2229270510f6c192c791b356bbdec1ae
Rafael Rivera <rafael@withinwindows.com>
2021-06-27 14:02:03 -0700

    Replace old build script

--- windows_app_sdk/build.rs
+++ windows_app_sdk/build.rs
@@ -1,18 +1,15 @@
-use std::{env, fs, path};
-
 fn main() {
-    let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-    println!("cargo:rustc-link-search=native=.windows/lib/{}", arch);
-
-    let source = format!(
-        ".windows/lib/{}/Microsoft.ProjectReunion.Bootstrap.dll",
-        arch
-    );
-    let target = path::Path::new(env::var("OUT_DIR").unwrap().as_str())
-        .join("../../../Microsoft.ProjectReunion.Bootstrap.dll");
-
-    fs::copy(&source, &target).unwrap();
-
+    let arch = match std::env::var("CARGO_CFG_TARGET_ARCH").unwrap().as_str() {
+        "x86_64" => "x64",
+        "x86" => "x86",
+        "arm" => "arm",
+        "aarch64" => "arm64",
+        unexpected => panic!(
+            "Unexpected `{}` architecture set by `CARGO_CFG_TARGET_ARCH`",
+            unexpected
+        ),
+    };
+    println!("cargo:rustc-link-search=native=.windows/{}", arch);
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=.windows");
 }
