7734b093ddc998e0b33ac7e92f0176ff8f04df39
Kenny Kerr <kekerr@microsoft.com>
2021-07-02 16:12:32 -0700

    Update Windows App SDK samples (#7)

--- windows_app_sdk/skeleton/src/main.rs
+++ windows_app_sdk/skeleton/src/main.rs
@@ -1,5 +1,8 @@
 #![windows_subsystem = "console"]
 
 fn main() -> windows::Result<()> {
-    windows_app::bootstrap::initialize()
+    windows_app::bootstrap::initialize()?;
+
+    println!("hello world");
+    Ok(())
 }
