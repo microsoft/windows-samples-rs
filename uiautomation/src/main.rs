3dc6dc21385f091fa43372c1dca09267dbb0b5a9
Ericvf <ericvf@fegelein.com>
2021-07-30 21:13:55 +0200

    Small improvements to the UIAutomation sample

--- uiautomation/src/main.rs
+++ uiautomation/src/main.rs
@@ -4,11 +4,25 @@ use Windows::Win32::System::Com::*;
 use Windows::Win32::UI::Accessibility::*;
 use Windows::Win32::UI::WindowsAndMessaging::*;
 use Windows::UI::UIAutomation::*;
+use Windows::Win32::System::SystemInformation::OSVERSIONINFOW;
+use Windows::Win32::Foundation::NTSTATUS;
 
 fn main() -> Result<()> {
-    unsafe {
-        CoInitializeEx(std::ptr::null_mut(), COINIT_MULTITHREADED)?;
+    unsafe {  
+        let version_info = get_version_info();
+        if version_info?.dwMajorVersion < 11 {
+            println!("This sample is intended to run on Windows 11.");
+            panic!()
+        }
+        
         let window = FindWindowA(None, "Calculator");
+        if window.is_null() 
+        {
+            println!("Calculator window not found. Please run calc.exe.");
+            panic!()
+        }
+        
+        CoInitializeEx(std::ptr::null_mut(), COINIT_MULTITHREADED)?;
 
         // Start with COM API
         let automation: IUIAutomation = CoCreateInstance(&CUIAutomation, None, CLSCTX_ALL)?;
@@ -18,7 +32,7 @@ fn main() -> Result<()> {
         let name = element.get_CurrentName()?;
         println!("window name: {}", name);
 
-        // Query for WinRT API (will fail on earlier versions of Windows)
+        // Query for WinRT API 
         let element: AutomationElement = element.cast()?;
 
         // Use WinRT API
@@ -26,4 +40,24 @@ fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+extern "system" {
+    fn RtlGetVersion(lpVersionInformation: *mut OSVERSIONINFOW) -> NTSTATUS;
+}
+
+fn get_version_info() -> Result<OSVERSIONINFOW>  {
+    unsafe {
+        let mut version_info = OSVERSIONINFOW {
+            dwOSVersionInfoSize: 0,
+            dwMajorVersion: 0,
+            dwMinorVersion: 0,
+            dwBuildNumber: 0,
+            dwPlatformId: 0,
+            szCSDVersion: [0; 128],
+        };
+
+        RtlGetVersion(&mut version_info);
+        return Ok(version_info)
+    }
 }
