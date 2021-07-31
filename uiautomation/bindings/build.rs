1bd377823b2112c11852211f970ebe34db2500a5
Ericvf <ericvf@fegelein.com>
2021-07-31 13:08:53 +0200

    Remove OS version check and formatted code

--- uiautomation/bindings/build.rs
+++ uiautomation/bindings/build.rs
@@ -5,7 +5,5 @@ fn main() {
         Windows::Win32::System::SystemServices::*,
         Windows::Win32::UI::WindowsAndMessaging::*,
         Windows::UI::UIAutomation::AutomationElement,
-        Windows::Win32::System::SystemInformation::OSVERSIONINFOW,
-        Windows::Win32::Foundation::NTSTATUS,
     );
 }
--- uiautomation/src/main.rs
+++ uiautomation/src/main.rs
@@ -4,24 +4,15 @@ use Windows::Win32::System::Com::*;
 use Windows::Win32::UI::Accessibility::*;
 use Windows::Win32::UI::WindowsAndMessaging::*;
 use Windows::UI::UIAutomation::*;
-use Windows::Win32::System::SystemInformation::OSVERSIONINFOW;
-use Windows::Win32::Foundation::NTSTATUS;
 
 fn main() -> Result<()> {
-    unsafe {  
-        let version_info = get_version_info();
-        if version_info?.dwMajorVersion < 11 {
-            println!("This sample is intended to run on Windows 11.");
-            panic!()
-        }
-        
+    unsafe {
         let window = FindWindowA(None, "Calculator");
-        if window.is_null() 
-        {
+        if window.is_null() {
             println!("Calculator window not found. Please run calc.exe.");
             panic!()
         }
-        
+
         CoInitializeEx(std::ptr::null_mut(), COINIT_MULTITHREADED)?;
 
         // Start with COM API
@@ -32,32 +23,12 @@ fn main() -> Result<()> {
         let name = element.get_CurrentName()?;
         println!("window name: {}", name);
 
-        // Query for WinRT API 
-        let element: AutomationElement = element.cast()?;
+        // Query for WinRT API (will fail on earlier versions of Windows)
+        let element2: AutomationElement = element.cast()?;
 
         // Use WinRT API
-        println!("file name: {}", element.ExecutableFileName()?);
+        println!("file name: {}", element2.ExecutableFileName()?);
     }
 
     Ok(())
-}
-
-extern "system" {
-    fn RtlGetVersion(lpVersionInformation: *mut OSVERSIONINFOW) -> NTSTATUS;
-}
-
-fn get_version_info() -> Result<OSVERSIONINFOW>  {
-    unsafe {
-        let mut version_info = OSVERSIONINFOW {
-            dwOSVersionInfoSize: 0,
-            dwMajorVersion: 0,
-            dwMinorVersion: 0,
-            dwBuildNumber: 0,
-            dwPlatformId: 0,
-            szCSDVersion: [0; 128],
-        };
-
-        RtlGetVersion(&mut version_info);
-        return Ok(version_info)
-    }
 }
