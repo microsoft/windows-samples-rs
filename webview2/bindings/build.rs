03c512b70b0c6c214c1f1603cf8c68d540dc3310
Kenny Kerr <kenny@kennykerr.ca>
2021-11-01 14:44:01 -0700

    Version24

--- webview2/bindings/build.rs
+++ webview2/bindings/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    windows::build! {
+    windows::runtime::build! {
         Microsoft::Web::WebView2::Core::*,
         Windows::Foundation::*,
         Windows::Win32::Foundation::{HINSTANCE, LRESULT, POINT, PWSTR, RECT, SIZE},
@@ -8,7 +8,7 @@ fn main() {
         Windows::Win32::System::LibraryLoader::GetModuleHandleA,
         Windows::Win32::System::Threading::GetCurrentThreadId,
         Windows::Win32::UI::HiDpi::SetProcessDpiAwareness,
-        Windows::Win32::UI::KeyboardAndMouseInput::SetFocus,
+        Windows::Win32::UI::Input::KeyboardAndMouse::SetFocus,
         Windows::Win32::UI::WindowsAndMessaging::*,
     };
 }
--- webview2_win32/readme.md
+++
@@ -1,1 +0,0 @@
-![WebView2](https://user-images.githubusercontent.com/9845234/135175934-09cb4656-c7f2-416c-b3ae-bd13b27feb85.gif)
