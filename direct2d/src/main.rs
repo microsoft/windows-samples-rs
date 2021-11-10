3d46b59f45546b6a6cbc6cd7f43f2aa5234e5993
Kenny Kerr <kenny@kennykerr.ca>
2021-11-10 08:28:30 -0800

    samples

--- direct2d/src/main.rs
+++ direct2d/src/main.rs
@@ -1,7 +1,9 @@
 use windows::{
-    runtime::*, Foundation::Numerics::*, Win32::Foundation::*, Win32::Graphics::Direct2D::*, Win32::Graphics::Direct2D::Common::*,
-    Win32::Graphics::Direct3D::*, Win32::Graphics::Direct3D11::*, Win32::Graphics::Dxgi::*, Win32::Graphics::Gdi::*, Win32::Graphics::Dxgi::Common::*,
-    Win32::System::Com::*, Win32::System::LibraryLoader::*, Win32::System::Performance::*,
+    runtime::*, Foundation::Numerics::*, Win32::Foundation::*,
+    Win32::Graphics::Direct2D::Common::*, Win32::Graphics::Direct2D::*,
+    Win32::Graphics::Direct3D::*, Win32::Graphics::Direct3D11::*, Win32::Graphics::Dxgi::Common::*,
+    Win32::Graphics::Dxgi::*, Win32::Graphics::Gdi::*, Win32::System::Com::*,
+    Win32::System::LibraryLoader::*, Win32::System::Performance::*,
     Win32::System::SystemInformation::GetLocalTime, Win32::UI::Animation::*,
     Win32::UI::WindowsAndMessaging::*,
 };
