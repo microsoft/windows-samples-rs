7993cfdb96f9dce3dcd0eb3ef38f4f877bb0f0d6
Kenny Kerr <kekerr@microsoft.com>
2021-11-19 11:42:12 -0800

    Update WebView2 sample to use `build` macro redirection (#41)

--- webview2/bindings/build.rs
+++ webview2/bindings/build.rs
@@ -1,14 +1,5 @@
 fn main() {
     windows::core::build! {
         Microsoft::Web::WebView2::Core::*,
-        Windows::Foundation::*,
-        Windows::Win32::Foundation::{HINSTANCE, LRESULT, POINT, PWSTR, RECT, SIZE},
-        Windows::Win32::Graphics::Gdi::UpdateWindow,
-        Windows::Win32::System::Com::{CoCreateInstance, CoInitializeEx},
-        Windows::Win32::System::LibraryLoader::GetModuleHandleA,
-        Windows::Win32::System::Threading::GetCurrentThreadId,
-        Windows::Win32::UI::HiDpi::SetProcessDpiAwareness,
-        Windows::Win32::UI::Input::KeyboardAndMouse::SetFocus,
-        Windows::Win32::UI::WindowsAndMessaging::*,
     };
 }
