a369b02f3c46bb44587f436959ee55acc3f4e446
Casper Verhaar <casper.verhaar@gmail.com>
2021-12-31 11:30:18 +0100

    rss: fix sample

--- rss/src/main.rs
+++ rss/src/main.rs
@@ -1,10 +1,27 @@
-use windows::{Foundation::Uri, Web::Syndication::SyndicationClient};
+use std::sync::mpsc;
+use windows::{
+    core::HSTRING,
+    Foundation::{AsyncOperationWithProgressCompletedHandler, Uri},
+    Web::Syndication::SyndicationClient,
+};
 
 fn main() -> windows::core::Result<()> {
-    let uri = Uri::CreateUri("https://kennykerr.ca/feed")?;
+    let uri = Uri::CreateUri(HSTRING::from("https://kennykerr.ca/feed"))?;
     let client = SyndicationClient::new()?;
-    let feed = client.RetrieveFeedAsync(uri)?.get()?;
 
+    let (sender, receiver) = mpsc::channel();
+    client.RetrieveFeedAsync(uri)?.SetCompleted(
+        AsyncOperationWithProgressCompletedHandler::new(move |op, _status| {
+            if let Some(op) = op {
+                sender
+                    .send(op.GetResults()?)
+                    .expect("send over mpsc channel");
+            }
+            Ok(())
+        }),
+    )?;
+
+    let feed = receiver.recv().unwrap();
     for item in feed.Items()? {
         println!("{}", item.Title()?.Text()?);
     }
